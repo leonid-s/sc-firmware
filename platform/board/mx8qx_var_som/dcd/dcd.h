9ad347df1cad6c09d2d58ec6d9dbbb217b3a42c4
Leonid Segal <leonid.s@variscite.com>
2020-02-25 16:28:42 +0200

    Initial git commit

---
+++ platform/board/mx8qx_var_som/dcd/dcd.h
@@ -0,0 +1,4 @@
+#ifndef DCD_H
+#define DCD_H
+#include "imx8qx_dcd_1.2GHz.h"
+#endif
---
+++ platform/board/mx8qx_var_som/dcd/dcd_retention.h
@@ -0,0 +1,4 @@
+#ifndef DDR_RETENTION_H
+#define DDR_RETENTION_H
+#include "imx8qx_dcd_1.2GHz_retention.h"
+#endif
